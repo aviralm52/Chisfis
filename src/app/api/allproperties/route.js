fcec8d11b163615b724e9882cf58eb83bd30e3cc
Aman <amantrivedi598@gmail.com>
2024-08-09 23:04:48 -0700

    resolved conflicts in allproperties/route.js

--- src/app/api/allproperties/route.js
+++ src/app/api/allproperties/route.js
@@ -1,36 +1,27 @@
 import { connectDb } from "../../../helper/db";
 import { NextRequest, NextResponse } from "next/server";
-import {NextApiRequest} from "next";
+import { NextApiRequest } from "next";
 import { Property } from "@/models/listing";
 
 connectDb();
 
 export async function GET(req) {
+  const url = req.url;
+  const urlList = url.split("limit=");
+  const limit = urlList[1];
+  const queryLimit = limit ? parseInt(limit, 10) : undefined;
 
-    const url = req.url;
-    const urlList = url.split('limit=');
-    const limit = urlList[1];
-    const queryLimit = limit ? parseInt(limit, 10): undefined;
+  try {
+    const allProperties = await Property.aggregate([
+      { $sample: { size: queryLimit || 0 } },
+    ]); // 0 means no limit
 
-    try {
-<<<<<<< HEAD
-        const allProperties = await Property.find().limit(4);
-=======
-        // const allProperties = await Property.find().limit(queryLimit || 0);     // 0 means no limit
-        const allProperties = await Property.aggregate([
-            {$sample: {size: queryLimit || 0}}
-        ]);     // 0 means no limit
->>>>>>> 980138046dd5ef208e08ec5f6ac5e8467d6339a6
+    return NextResponse.json(allProperties);
+  } catch (error) {
+    console.error("Error fetching properties: ", error);
 
-        return NextResponse.json(allProperties);
-    } catch (error) {
-        console.error("Error fetching properties: ", error);
-
-        return NextResponse.json({
-            message: "Failed to fetch properties from the database",
-        });
-    }
+    return NextResponse.json({
+      message: "Failed to fetch properties from the database",
+    });
+  }
 }
-
-
-
