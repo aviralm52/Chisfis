b9b44996e0133354b7ffb5675036e33d9d0757a2
Aman <amantrivedi598@gmail.com>
2024-08-14 05:20:54 -0700

    done with short/long term filter api

--- src/app/api/allproperties/route.js
+++ src/app/api/allproperties/route.js
@@ -7,7 +7,7 @@ connectDb();
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
   const limit = parseInt(searchParams.get("limit") || "12", 10);
-  const page = parseInt(searchParams.get("page") || "1", 10);
+  const page = parseInt(searchParams.get("page") || "1", 10); 
   const skip = (page - 1) * limit;
   const rentalType = searchParams.get("rentalType");
 
